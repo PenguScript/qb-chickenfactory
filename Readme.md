6a5bc28978b0e23d287d82cba910e9f8b6c95b77
Not-Pengu <102926606+Not-Pengu@users.noreply.github.com>
2022-08-02 19:04:30 -0400

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,98 @@
+# A Chicken Factory Job for QBCore Framework
+
+-- I would love to thank ooSayeroo for this wonder script I used as a backbone for this script. You may find his script below:
+
+-- Reworked from ooSayeroo's script for the Legion Square Weed Clinic to a Chicken Factory in Paleto Bay (NO MLOs REQUIRED)
+
+## Please note
+
+- Please make sure u use the latest dependencies aswell as core for this in order to work.
+
+
+## Dependencies :
+
+QBCore Framework - https://github.com/qbcore-framework/qb-core
+
+PolyZone - https://github.com/mkafrin/PolyZone
+
+qb-target - https://github.com/BerkieBb/qb-target
+
+qb-input - https://github.com/qbcore-framework/qb-input
+
+qb-menu - https://github.com/qbcore-framework/qb-menu
+
+
+## Credits : 
+
+ooSayeroo for the script of which I used as a backbone.
+
+## Insert the following into @qb-core/shared/items.lua 
+
+```
+QBShared.Items = {
+-- Chicken Factory Shop
+['chickennuggets'] 			    	 = {['name'] = 'chickennuggets', 			    	['label'] = 'Chicken Nuggets', 		    	['weight'] = 50, 		['type'] = 'item', 		["image"] = "chickennuggets.png",       	["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "Yummy Nuggies."},
+['cleanchicken'] 			    	 = {['name'] = 'cleanchicken', 			        	['label'] = 'Clean Chicken', 		    	['weight'] = 200, 		['type'] = 'item', 		["image"] = "cleanchicken.png",           	["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "Sparkling Clean Chicken."},
+['groundchicken'] 				     = {['name'] = 'groundchicken', 			        ['label'] = 'Ground Chicken', 		     	['weight'] = 200, 		['type'] = 'item', 		['image'] = 'groundchicken.png',        	['unique'] = false, 	['useable'] = true, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = 'I Just Ground Up My Own Chicken!'},
+['dirtychicken'] 		 	 	 	 = {['name'] = 'dirtychicken',           			['label'] = 'Dirty Chicken',	 	    	['weight'] = 200, 		['type'] = 'item', 		["image"] = "dirtychicken.png", 		    ["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "This Smells HORRIBLE!"},
+['breadcrumbs'] 			 	     = {['name'] = 'breadcrumbs', 			  	        ['label'] = 'Breadcrumbs', 			        ['weight'] = 50, 		['type'] = 'item', 		['image'] = 'breadcrumbs.png', 	        	['unique'] = false, 	['useable'] = true, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = 'A Great Thing For Chicken Nuggies.'},
+['receipt'] 			        	 = {['name'] = 'receipt', 			                ['label'] = 'Receipt', 			            ['weight'] = 50, 		['type'] = 'item', 		['image'] = 'receipt.png', 	                ['unique'] = false, 	['useable'] = true, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = 'receipt!'},
+['eggs'] 			 	             = {['name'] = 'eggs', 			                	['label'] = 'Eggs', 		            	['weight'] = 10, 		['type'] = 'item', 		['image'] = 'eggs.png', 		            ['unique'] = false, 	['useable'] = true, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = 'Don't eat them raw!'},
+}
+
+```
+
+## Insert EVERYTHING in @chickenfactory/Images into @qb-inventory/HTML/Images
+
+
+## Insert the following into @qb-core/shared/jobs.lua 
+```
+QBShared.Jobs = {
+    ["chickenfactory"] = {
+		label = "Chicken Factory Employee",
+		defaultDuty = true,
+		grades = {
+            ['0'] = {
+                name = "Trainee",
+                payment = 100
+            },
+			['1'] = {
+                name = "Employee",
+                payment = 150
+            },
+			['2'] = {
+                name = "Owner",
+                isboss = true,
+                payment = 300
+            },
+        },
+	},
+}		
+```
+
+## Insert the following into @qb-management - Client - cl config.lua
+```
+['chickenfactory'] = {
+        vector3(-67.53, 6254.06, 30.09),
+    },
+```
+
+## Insert the following into @qb-target - init.lua - config.targetmodels
+``` 
+["chickenfactorygarage"] = {
+		models = {
+			"s_m_y_factory_01"
+		},
+		options = {
+			{
+				type = "client",
+				event = "garage:ChickenFactoryGarage",
+				icon = "far fa-clipboard",
+				label = "ChickenFactory Garage",
+				job = "chickenfactory",
+			}
+		},
+		distance = 2.5,
+	},
+
+```
---
+++ client/deliveries.lua
@@ -0,0 +1,288 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+isLoggedIn = false
+local onDuty = true
+PlayerJob = {}
+
+RegisterNetEvent('qb-chickenfactory:deliveries:DeliverEggs', function()
+    TriggerEvent('animations:client:EmoteCommandStart', {"type"})
+    QBCore.Functions.Progressbar('falar_empregada', 'Getting Delivery...', 5000, false, true, {
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {}, {}, {}, function()
+    TriggerEvent('animations:client:EmoteCommandStart', {"c"})
+    QBCore.Functions.Notify('You Accepted a delivery! It should appear in your emails soon!', 'primary', 7500)
+    
+    Wait(Config.DeliveryWait)
+
+    TriggerServerEvent('qb-phone:server:sendNewMail', {
+        sender = 'Automated Assistance',
+        subject = 'Delivery of Eggs...',
+        message = 'Delivery has been accepted by company. please deliver the -- Eggs -- to the customer.',
+        })
+    TriggerServerEvent('qb-chickenfactory:server:DeliveryItem')
+    startdropoff()
+    end)
+end)
+
+RegisterNetEvent('qb-chickenfactory:deliveries:KnockDoor', function()
+    TriggerEvent('animations:client:EmoteCommandStart', {"knock"})
+    QBCore.Functions.Progressbar('falar_empregada', 'Knocking Door...', 5000, false, true, {
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {}, {}, {}, function()
+    QBCore.Functions.Notify('Delivery Successful, return to the shop', 'primary', 7500)
+
+    TriggerServerEvent('qb-chickenfactory:server:KnockDoor')
+    end)
+end)
+
+RegisterNetEvent("qb-chickenfactory:deliveries:ReceivePayment")
+AddEventHandler("qb-chickenfactory:deliveries:ReceivePayment", function()
+    if onDuty then
+    	QBCore.Functions.TriggerCallback('qb-chickenfactory:server:get:ReceiptChecker', function(HasItems)  
+    		if HasItems then
+				QBCore.Functions.Progressbar("pickup_sla", "Filing Receipt..", 4000, false, true, {
+					disableMovement = true,
+					disableCarMovement = true,
+					disableMouse = false,
+					disableCombat = true,
+				}, {
+					animDict = "mp_common",
+					anim = "givetake1_a",
+					flags = 8,
+				}, {}, {}, function() -- Done
+					TriggerServerEvent('qb-chickenfactory:server:ReceivePayment')
+					TriggerServerEvent('QBCore:Server:RemoveItem', "customer-receipt", 1)
+							QBCore.Functions.Notify("You Filed a Receipt", "success")
+				end, function()
+					QBCore.Functions.Notify("Cancelled..", "error")
+				end)
+			else
+   				QBCore.Functions.Notify("You dont have the right stuff to make this", "error")
+			end
+		end)
+	else 
+		QBCore.Functions.Notify("You must be Clocked into work", "error")
+	end
+end)
+
+
+function startdropoff()
+    local prob = math.random(1, 10)
+
+    if prob == 1 then
+        SetNewWaypoint(Config.DropOffPoint1)
+        startdropoff1()
+    elseif prob == 2 then
+        SetNewWaypoint(Config.DropOffPoint2)
+        startdropoff2()
+    elseif prob == 3 then
+        SetNewWaypoint(Config.DropOffPoint3)
+        startdropoff3()
+    elseif prob == 4 then
+        SetNewWaypoint(Config.DropOffPoint4)
+        startdropoff4()
+    elseif prob == 5 then
+        SetNewWaypoint(Config.DropOffPoint5)
+        startdropoff5()
+    elseif prob == 6 then
+        SetNewWaypoint(Config.DropOffPoint6)
+        startdropoff6()
+    elseif prob == 7 then
+        SetNewWaypoint(Config.DropOffPoint7)
+        startdropoff7()
+    elseif prob == 8 then
+        SetNewWaypoint(Config.DropOffPoint8)
+        startdropoff8()
+    elseif prob == 9 then
+        SetNewWaypoint(Config.DropOffPoint9)
+        startdropoff9()
+    elseif prob == 10 then
+        SetNewWaypoint(Config.DropOffPoint10)
+        startdropoff10()
+    end
+end
+
+function startdropoff1()
+    exports['qb-target']:AddBoxZone("eggs", Config.DropOffPoint1, 1, 1, {
+        name="eggs",
+        heading=0,
+        debugpoly = false,
+    }, {
+        options = {
+            {
+            event = "qb-chickenfactory:deliveries:KnockDoor",
+            icon = "far fa-box",
+            label = "Knock Door",
+            item = "eggs",
+            },
+        },
+        distance = 2.5
+    })
+end
+
+function startdropoff2()
+    exports['qb-target']:AddBoxZone("eggs", Config.DropOffPoint2, 1, 1, {
+        name="eggs",
+        heading=0,
+        debugpoly = false,
+    }, {
+        options = {
+            {
+            event = "qb-chickenfactory:deliveries:KnockDoor",
+            icon = "far fa-box",
+            label = "Knock Door",
+            item = "eggs",
+            },
+        },
+        distance = 2.5
+    })
+end
+
+function startdropoff3()
+    exports['qb-target']:AddBoxZone("eggs", Config.DropOffPoint3, 1, 1, {
+        name="eggs",
+        heading=0,
+        debugpoly = false,
+    }, {
+        options = {
+            {
+            event = "qb-chickenfactory:deliveries:KnockDoor",
+            icon = "far fa-box",
+            label = "Knock Door",
+            item = "eggs",
+            },
+        },
+        distance = 2.5
+    })
+end
+
+function startdropoff4()
+    exports['qb-target']:AddBoxZone("eggs", Config.DropOffPoint4, 1, 1, {
+        name="eggs",
+        heading=0,
+        debugpoly = false,
+    }, {
+        options = {
+            {
+            event = "qb-chickenfactory:deliveries:KnockDoor",
+            icon = "far fa-box",
+            label = "Knock Door",
+            item = "eggs",
+            },
+        },
+        distance = 2.5
+    })
+end
+
+function startdropoff5()
+    exports['qb-target']:AddBoxZone("eggs", Config.DropOffPoint5, 1, 1, {
+        name="eggs",
+        heading=0,
+        debugpoly = false,
+    }, {
+        options = {
+            {
+            event = "qb-chickenfactory:deliveries:KnockDoor",
+            icon = "far fa-box",
+            label = "Knock Door",
+            item = "eggs",
+            },
+        },
+        distance = 2.5
+    })
+end
+
+function startdropoff6()
+    exports['qb-target']:AddBoxZone("eggs", Config.DropOffPoint6, 1, 1, {
+        name="eggs",
+        heading=0,
+        debugpoly = false,
+    }, {
+        options = {
+            {
+            event = "qb-chickenfactory:deliveries:KnockDoor",
+            icon = "far fa-box",
+            label = "Knock Door",
+            item = "eggs",
+            },
+        },
+        distance = 2.5
+    })
+end
+
+function startdropoff7()
+    exports['qb-target']:AddBoxZone("eggs", Config.DropOffPoint7, 1, 1, {
+        name="eggs",
+        heading=0,
+        debugpoly = false,
+    }, {
+        options = {
+            {
+            event = "qb-chickenfactory:deliveries:KnockDoor",
+            icon = "far fa-box",
+            label = "Knock Door",
+            item = "eggs",
+            },
+        },
+        distance = 2.5
+    })
+end
+
+function startdropoff8()
+    exports['qb-target']:AddBoxZone("eggs", Config.DropOffPoint8, 1, 1, {
+        name="eggs",
+        heading=0,
+        debugpoly = false,
+    }, {
+        options = {
+            {
+            event = "qb-chickenfactory:deliveries:KnockDoor",
+            icon = "far fa-box",
+            label = "Knock Door",
+            item = "eggs",
+            },
+        },
+        distance = 2.5
+    })
+end
+
+function startdropoff9()
+    exports['qb-target']:AddBoxZone("eggs", Config.DropOffPoint9, 1, 1, {
+        name="eggs",
+        heading=0,
+        debugpoly = false,
+    }, {
+        options = {
+            {
+            event = "qb-chickenfactory:deliveries:KnockDoor",
+            icon = "far fa-box",
+            label = "Knock Door",
+            item = "eggs",
+            },
+        },
+        distance = 2.5
+    })
+end
+
+function startdropoff10()
+    exports['qb-target']:AddBoxZone("eggs", Config.DropOffPoint10, 1, 1, {
+        name="eggs",
+        heading=0,
+        debugpoly = false,
+    }, {
+        options = {
+            {
+            event = "qb-chickenfactory:deliveries:KnockDoor",
+            icon = "far fa-box",
+            label = "Knock Door",
+            item = "eggs",
+            },
+        },
+        distance = 2.5
+    })
+end
